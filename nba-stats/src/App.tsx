12b0ef5347814f56257e37d2a2dd6f0d025040b2
QuinnB11 <butcher.github@gmail.cm>
2024-12-08 21:39:59 -0500

    Fix query client provider

--- nba-stats/src/App.tsx
+++ nba-stats/src/App.tsx
@@ -3,13 +3,10 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import {MainPage} from './pages/MainPage'
 
-const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
         <MainPage />
-    </QueryClientProvider>
   );
 }
 
--- nba-stats/src/index.tsx
+++ nba-stats/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
+const queryClient = new QueryClient();
+
 
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
   </React.StrictMode>
 );
